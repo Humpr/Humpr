0bf569012efe9e3fde0aa327f0c04d1db91ba4d9
Andre Julius <noromoron@gmail.com>
2019-02-14 02:14:16 +0100

    Replace Link with NavLink. Tune padding and border values.

--- src/frontend/src/components/Navigation/Navigation.js
+++ src/frontend/src/components/Navigation/Navigation.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import style from "./Navigation.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 class Navigation extends Component {
     render() {
         return (
             <nav className={style.navigation}>
-                <Link to="following"><button className={[style.link, style.active].join(" ")}>Following</button></Link>
-                <Link to="trending"><button className={style.link}>Trending</button></Link>
-                <Link to="recommended"><button className={style.link}>Recommended</button></Link>
+                <NavLink className={style.link} activeClassName={style.active} to="/following">Following</NavLink>
+                <NavLink className={style.link} activeClassName={style.active} to="/trending">Trending</NavLink>
+                <NavLink className={style.link} activeClassName={style.active} to="/recommended">Recommended</NavLink>
             </nav>
         );
     }
