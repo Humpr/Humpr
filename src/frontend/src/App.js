3be11576278373a602f766e2e724b77f32b5e351
Andre Julius <noromoron@gmail.com>
2019-02-14 02:15:50 +0100

    Redirect / to /following

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom"
 import Header from "./components/Header/Header";
 import "./App.css";
 
@@ -6,6 +7,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <Redirect exact from="/" to="/following" />
                 <Header></Header>
             </div>
         );
