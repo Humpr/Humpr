d5c57bd14b9d717cc729202e79d4d5e2767df1f1
Andre Julius <noromoron@gmail.com>
2019-02-16 04:38:49 +0100

    Use parent styling on profil icons if available. Use display: block as default

--- src/frontend/src/components/ProfileIcon/ProfileIcon.js
+++ src/frontend/src/components/ProfileIcon/ProfileIcon.js
@@ -5,7 +5,9 @@ import placeholder from "./placeholder_400x400.jpg";
 class ProfileIcon extends Component {
     render() {
         return (
-            <div><img className={style.img} alt="" src={placeholder}/></div>
+            <div className={this.props.containerClassName || ""}>
+                <img className={this.props.className || style.img} alt="" src={placeholder}/>
+            </div>
         );
     }
 }
