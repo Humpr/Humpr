1170af39b8194af9b13fde0c0517132ddc40cffe
Fred <helloworldwhatisup@gmail.com>
2019-03-05 21:01:25 -0500

    Added symbols for re-hump and heart in SVG's.

--- src/frontend/src/components/FollowingCard/FollowingCard.js
+++ src/frontend/src/components/FollowingCard/FollowingCard.js
@@ -14,8 +14,16 @@ class FollowingCard extends Component {
                     <div className={style.cardUername}>Username</div>
                     <div className={style.cardIcons}>
                         <div className={style.iconDot}>·</div>
-                        <div className={style.iconReHump}>[]</div>
-                        <div className={style.iconHump}>&lt;3</div>
+                        <div className={style.iconReHump}>
+                        <object type="image/svg+xml" width="80%" height="80%" data="https://trello-attachments.s3.amazonaws.com/5c0b0627b732e12622071af5/5c0f25ec6ce3946de706e219/20fd37e83724329c0ca0197427e44652/Re-Hump.svg">
+                          <img src="https://trello-attachments.s3.amazonaws.com/5c0b0627b732e12622071af5/5c0f25ec6ce3946de706e219/e00bf82f2c5a9d718fd3871491849f01/Re-Hump.png" />
+                        </object>
+                        </div>
+                        <div className={style.iconHump}>
+                        <object type="image/svg+xml" width="90%" height="90%" data="https://trello-attachments.s3.amazonaws.com/5c0b0627b732e12622071af5/5c0b10ec43c3d149acb9343e/cafa8cf9ede58f5bdaa0f98b51c64030/Heart_lines.svg">
+                          <img src="https://trello-attachments.s3.amazonaws.com/5c0b0627b732e12622071af5/5c0b10ec43c3d149acb9343e/c40c866230c6d4d195a4a8d965d15c73/Heart_lines.png" />
+                        </object>
+                        </div>
                     </div>
                 </div>
                 <div className={style.cardContentContainer}>
